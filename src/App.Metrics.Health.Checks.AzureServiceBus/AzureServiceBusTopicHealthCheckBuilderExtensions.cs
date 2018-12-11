c1ad415abbffed005ac3497c48ac2d30b211415d
Benney Au <benney.au@prospa.com>
2018-12-11 12:39:23 +1100

    add azure service bus DLQ check

--- src/App.Metrics.Health.Checks.AzureServiceBus/AzureServiceBusTopicHealthCheckBuilderExtensions.cs
+++ src/App.Metrics.Health.Checks.AzureServiceBus/AzureServiceBusTopicHealthCheckBuilderExtensions.cs
@@ -5,8 +5,10 @@
 using System;
 using System.Text;
 using System.Threading.Tasks;
+using App.Metrics.Health.Checks.AzureServiceBus;
 using App.Metrics.Health.Logging;
 using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Management;
 
 // ReSharper disable CheckNamespace
 namespace App.Metrics.Health
@@ -65,6 +67,62 @@ namespace App.Metrics.Health
             builder.AddCheck(name, CheckServiceBusTopicConnectivity(name, topicClient));
 
             return builder.Builder;
+        }
+
+        public static IHealthBuilder AddAzureServiceBusTopicSubscriptionDeadLetterQueueCheck(
+            this IHealthCheckBuilder builder,
+            string name,
+            string connectionString,
+            string topicName,
+            string subscriptionName,
+            long deadLetterWarningThreshold = 1,
+            long? deadLetterErrorThreshold = null)
+        {
+            if (deadLetterErrorThreshold.HasValue && (deadLetterWarningThreshold > deadLetterErrorThreshold))
+            {
+                throw new ArgumentException("Error threshold must exceed warning threshold", nameof(deadLetterErrorThreshold));
+            }
+
+            var managementClient = new ManagementClient(connectionString);
+            builder.AddCheck(
+                name,
+                ServiceBusHealthChecks.CheckDeadLetterQueueCount(Logger, EntityNameHelper.FormatSubscriptionPath(topicName, subscriptionName), name, GetQueueMessageCount, deadLetterWarningThreshold, deadLetterErrorThreshold));
+            return builder.Builder;
+
+            async Task<MessageCountDetails> GetQueueMessageCount()
+            {
+                var info = await managementClient.GetSubscriptionRuntimeInfoAsync(topicName, subscriptionName);
+                return info.MessageCountDetails;
+            }
+        }
+
+        public static IHealthBuilder AddAzureServiceBusTopicSubscriptionDeadLetterQueueCheck(
+            this IHealthCheckBuilder builder,
+            string name,
+            string connectionString,
+            string topicName,
+            string subscriptionName,
+            TimeSpan cacheDuration,
+            long deadLetterWarningThreshold = 1,
+            long? deadLetterErrorThreshold = null)
+        {
+            if (deadLetterErrorThreshold.HasValue && (deadLetterWarningThreshold > deadLetterErrorThreshold))
+            {
+                throw new ArgumentException("Error threshold must exceed warning threshold", nameof(deadLetterErrorThreshold));
+            }
+
+            var managementClient = new ManagementClient(connectionString);
+            builder.AddCachedCheck(
+                name,
+                ServiceBusHealthChecks.CheckDeadLetterQueueCount(Logger, EntityNameHelper.FormatSubscriptionPath(topicName, subscriptionName), name, GetQueueMessageCount, deadLetterWarningThreshold, deadLetterErrorThreshold),
+                cacheDuration);
+            return builder.Builder;
+
+            async Task<MessageCountDetails> GetQueueMessageCount()
+            {
+                var info = await managementClient.GetSubscriptionRuntimeInfoAsync(topicName, subscriptionName);
+                return info.MessageCountDetails;
+            }
         }
 
         private static Func<ValueTask<HealthCheckResult>> CheckServiceBusTopicConnectivity(string name, TopicClient topicClient)
